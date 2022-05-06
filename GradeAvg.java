0a46a632a03e403043c57b1a60152c749bc456f7
Clouvis Denosta <clouvis.denosta@norimaconsulting.com>
2022-05-06 09:27:21 +0800

    Try with resources code

--- GradeAvg.java
+++ GradeAvg.java
@@ -6,17 +6,18 @@ public class GradeAvg {
     public static void main (String []args){
         NumberFormat percent = NumberFormat.getPercentInstance();
         HashMap<String, Integer> grades = new HashMap<String, Integer>();
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter grade in Math: ");
-        grades.put("Math", sc.nextInt());
-        System.out.print("Enter grade in Filipino: ");
-        grades.put("Filipino", sc.nextInt());
-        System.out.print("Enter grade in Science: ");
-        grades.put("Science", sc.nextInt());
-        System.out.print("Enter grade in English: ");
-        grades.put("English", sc.nextInt());
-        System.out.print("Enter grade in Social: ");
-        grades.put("Social", sc.nextInt());
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter grade in Math: ");
+            grades.put("Math", sc.nextInt());
+            System.out.print("Enter grade in Filipino: ");
+            grades.put("Filipino", sc.nextInt());
+            System.out.print("Enter grade in Science: ");
+            grades.put("Science", sc.nextInt());
+            System.out.print("Enter grade in English: ");
+            grades.put("English", sc.nextInt());
+            System.out.print("Enter grade in Social: ");
+            grades.put("Social", sc.nextInt());
+        }
         System.out.println("==================================\n");
         System.out.format("%-10s %15s", "Subject", "Grade\n");
         float sum = 0.0f;
