b3e06805262e50a50d408cc85f24fc32b1f4db6e
Clouvis Denosta <clouvis.denosta@norimaconsulting.com>
2022-05-06 08:06:35 +0800

    initial code pizza cost program

---
+++ PizzaCost.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+public class PizzaCost {
+    public static void main(String [] args){
+        HashMap<String, Double> in = new HashMap<String, Double>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the diameter of the pizza is: ");
+        in.put("inches", sc.nextDouble());
+        for(Map.Entry<String, Double> inches : in.entrySet()) {
+            double labor_cost = 0.75, rent_cost = 1.00;
+            double mat_cost = 0.05 * (inches.getValue()*inches.getValue());
+            double cost = mat_cost + rent_cost + labor_cost;
+            System.out.println("The cost of making the pizza is: $"+ cost);
+        }
+    }
+}
